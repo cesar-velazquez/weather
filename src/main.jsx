d9bdcb2e44232a2d2f415d145b86fe4ac150a849
cesar-velazquez <velazquezortizcesar@gmail.com>
2023-08-08 09:45:00 -0600

    bug loader v2

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,5 @@ window.addEventListener('load', function () {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-    document.getElementById('root')
+  </React.StrictMode>,    
 )
