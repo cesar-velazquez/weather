e51bfaefaf429ff043ead859d69b8e1febe9fd60
cesar-velazquez <velazquezortizcesar@gmail.com>
2023-08-08 09:43:23 -0600

    bug loader v2

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,10 +1,9 @@
-function loader() {
-    setTimeout(function () {
-        document
-            .querySelector(".content__loader")
-            .classList.add("content__loader__hidden");        
-    }, 1000);
+export function loader() {
+  setTimeout(function () {
+    document.querySelector('.content__loader').classList.add('content__loader__hidden');
+  }, 1000);
 }
+
 window.addEventListener("load", function () {
   loader();
 });
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { loader } from './components/loader'; 
+
+window.addEventListener('load', function () {
+  loader(); // Llama a la función loader después de que la página se haya cargado
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+    document.getElementById('root')
 )
