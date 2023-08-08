faf3f00e685a86a676ef45f2453258c02c34f503
cesar-velazquez <velazquezortizcesar@gmail.com>
2023-08-08 02:27:58 -0600

    loader fix

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,18 +1,11 @@
-// document.documentElement.classList.add('dark')
-
-// const changeMode  = () =>{
-//     document.documentElement.classList.add("dark");
-// }
-
 function loader() {
     setTimeout(function () {
         document
             .querySelector(".content__loader")
             .classList.add("content__loader__hidden");        
-    }, 1000);
+    }, 500);
 }
 
 window.addEventListener("load", function () {
     loader();
 });
-
